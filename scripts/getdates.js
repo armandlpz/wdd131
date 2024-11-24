3d7723e6b452a8c3127b4284825e549beb626c2b
armandlpz <161987844+armandlpz@users.noreply.github.com>
2024-11-23 23:46:19 -0600

    hn

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const $year = document.querySelector("#currentyear");
+
+const $lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+$year.innerHTML = today.getFullYear();
+$lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
